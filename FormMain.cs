492e8070c11a9b919577381a84d18f1a0e0073b0
anju <s2309@e-mirim.hs.kr>
2025-04-22 16:09:17 +0900

    코드 리팩토링 -> 기본 파일명, 기본 필터명 상수로

--- FormMain.cs
+++ FormMain.cs
@@ -14,10 +14,14 @@ namespace HelloWorldWinform
     public partial class FormMain : Form
     {
         private static string OriginalText;
+        private const string DEFAULT_FILE_NAME = "제목없음";
+        private const string DEFAULT_FILE_FILTER = "텍스트 문서(*.txt)|*.txt|csv 파일(*.csv)|*.csv|모든 파일(*.*)|*.*";
+
         public FormMain()
         {
             InitializeComponent();
             OriginalText = textBox1.Text;
+            lblFileName.Text = DEFAULT_FILE_NAME;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -57,7 +61,7 @@ namespace HelloWorldWinform
         private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "텍스트 문서(*.txt)|*.txt|csv 파일(*.csv)|*.csv|모든 파일(*.*)|*.*";
+            openFileDialog.Filter = DEFAULT_FILE_FILTER;
             DialogResult result = openFileDialog.ShowDialog();
 
             switch (result)
@@ -86,7 +90,7 @@ namespace HelloWorldWinform
 
         private void 새로만들기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            lblFileName.Text = "제목없음";
+            lblFileName.Text = DEFAULT_FILE_NAME;
             textBox1.Text = "글자를 입력하세요~";
             OriginalText = textBox1.Text;
             lblTextChanged.Text = "";
@@ -94,10 +98,10 @@ namespace HelloWorldWinform
 
         private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (lblFileName.Text == "제목없음")
+            if (lblFileName.Text == DEFAULT_FILE_NAME)
             {
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "텍스트 문서(*.txt)|*.txt|csv 파일(*.csv)|*.csv|모든 파일(*.*)|*.*";
+                saveFileDialog.Filter = DEFAULT_FILE_FILTER;
                 DialogResult result = saveFileDialog.ShowDialog();
 
                 switch (result)
@@ -124,7 +128,7 @@ namespace HelloWorldWinform
         private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "텍스트 문서(*.txt)|*.txt|csv 파일(*.csv)|*.csv|모든 파일(*.*)|*.*";
+            saveFileDialog.Filter = DEFAULT_FILE_FILTER;
             saveFileDialog.FileName = lblFileName.Text;
             DialogResult result = saveFileDialog.ShowDialog();
 
